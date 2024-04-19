f5564ee159b52844570ab4f18b55ab4542782e61
mvstermind <zablockidawid1@gmail.com>
2024-04-19 23:05:16 +0200

    modular looks good now

--- cmds/templates/modular.go
+++ cmds/templates/modular.go
@@ -6,33 +6,35 @@ import (
 	"path/filepath"
 )
 
-// TODO: yep don't forget to clean this one 2
 func Modular() {
+	directories := []string{
+		"cmd",
+		"internal",
+		"pkg",
+		"api",
+		"web",
+		"scripts",
+		"configs",
+		"tests",
+		"docs",
+	}
 
-	currentUser, err := os.Getwd()
+	currentDir, err := os.Getwd()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	dir := filepath.Join(currentUser, "headstart/module1")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/cmd")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/internal")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/pkg")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/api")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/web")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/scripts")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/configs")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/tests")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module1/docs")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/module2")
-	err = os.MkdirAll(dir, 0750)
+
+	basePath := filepath.Join(currentDir, "headstart/module1")
+	for _, dir := range directories {
+		err := os.MkdirAll(filepath.Join(basePath, dir), 0750)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = os.MkdirAll(filepath.Join(currentDir, "headstart/module2"), 0750)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
