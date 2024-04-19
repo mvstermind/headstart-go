5fd05edefe500c4a110eb045df18131dcae274e6
mvstermind <zablockidawid1@gmail.com>
2024-04-19 23:20:42 +0200

    fixed layared

--- cmds/templates/layared.go
+++ cmds/templates/layared.go
@@ -1,56 +1,56 @@
 package templates
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 )
 
-// TODO: A LOT OF MESS IN HEREEEEEEEE TO CLEAN UP
 func Layared() {
-	currentUser, err := os.Getwd()
+	directories := []string{
+		"web",
+		"web/static",
+		"web/template",
+		"api",
+		"api/middleware",
+		"data",
+		"config",
+		"tests",
+		"docs",
+	}
+
+	current, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	dir := filepath.Join(currentUser, "headstart")
 
-	err = os.MkdirAll(dir, 0750)
+	baseDir := filepath.Join(current, "headstart")
 
+	err = os.MkdirAll(baseDir, 0750)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = os.WriteFile(fmt.Sprintf("%s/main.go", dir), []byte("package main\n\nfunc main() {\n\n}"), 0660)
 
-	dir = filepath.Join(currentUser, "headstart/web")
-	err = os.MkdirAll(dir, 0750)
-
+	err = os.WriteFile(filepath.Join(baseDir, "main.go"), []byte("package main\n\nfunc main() {\n\n}"), 0660)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = os.WriteFile(fmt.Sprintf("%s/handler.go", dir), []byte("package web\n\nfunc hanlder() {\n}"), 0660)
 
-	if err != nil {
-		log.Fatal(err)
+	for _, dir := range directories {
+		fullPath := filepath.Join(baseDir, dir)
+		err = os.MkdirAll(fullPath, 0750)
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch {
+		case dir == "web":
+			err = os.WriteFile(filepath.Join(fullPath, "handler.go"), []byte("package web\n\nfunc hanlder() {\n}"), 0660)
+		case dir == "api":
+			err = os.WriteFile(filepath.Join(fullPath, "routes.go"), []byte("package api\n\nfunc routes(){\n}"), 0660)
+		case dir == "data":
+			err = os.WriteFile(filepath.Join(fullPath, "database.go"), []byte("package data\n\nfunc database(){\n}"), 0660)
+		}
+		err = os.WriteFile(filepath.Join(fullPath, "repository.go"), []byte("package data\n\nfunc repository(){\n}"), 0660)
+
 	}
-	dir = filepath.Join(currentUser, "headstart/web/static")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/web/templates")
-	err = os.MkdirAll(dir, 0750)
-
-	dir = filepath.Join(currentUser, "headstart/api")
-	err = os.MkdirAll(dir, 0750)
-	err = os.WriteFile(fmt.Sprintf("%s/routes.go", dir), []byte("package api\n\nfunc routes(){\n}"), 0660)
-	dir = filepath.Join(currentUser, "headstart/api/middleware")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/data")
-	err = os.MkdirAll(dir, 0750)
-	err = os.WriteFile(fmt.Sprintf("%s/database.go", dir), []byte("package data\n\nfunc database(){\n}"), 0660)
-	err = os.WriteFile(fmt.Sprintf("%s/repository.go", dir), []byte("package data\n\nfunc repository(){\n}"), 0660)
-	dir = filepath.Join(currentUser, "headstart/configs")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/tests")
-	err = os.MkdirAll(dir, 0750)
-	dir = filepath.Join(currentUser, "headstart/docs")
-	err = os.MkdirAll(dir, 0750)
 }
